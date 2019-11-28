7f39094097d1a708c29de6f54d75571f297bf6ce
Denneth Dacara <denneth@gmail.com>
2019-11-28 13:51:06 +0800

    fixes

--- wp-content/themes/AesthetiQ-v1/index.php
+++ wp-content/themes/AesthetiQ-v1/index.php
@@ -1,3 +1,23 @@
-<?php
+<?php 
+    get_header(); 
+    global $wp;
+?>
 
-echo 'index';
+<div class="mt-5 pt-5"></div>
+
+<section id="our-signature-treatments" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 p-5">
+                <h1>Page <span class="font-weight-bold">"<?php echo basename(home_url( $wp->request )); ?>"</span> not found.</h1>
+                <p>
+                    <a href="<?php echo site_url(); ?>" class="text-dark">
+                        Back to Home
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
